cfc291604888e524ed14118b0442a6c25cdd06a1
Craig Buchanan <craig.buchanan@choa.org>
2015-06-22 20:52:00 -0400

    initial commit

---
+++ PsSharePoint.psm1
@@ -0,0 +1,12 @@
+Write-Host "Importing module PsSharePoint..."
+
+#
+# load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
+#
+Get-ChildItem "$PSScriptRoot\*.ps1" | 
+    Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
+    % { . $_ }
+
+Export-ModuleMember Add-Attachments
+
+Export-ModuleMember -Alias spaa
