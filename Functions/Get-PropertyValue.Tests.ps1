5b1db4ec9b4fcc561d1f0a450df9a4865a4a364a
Craig Buchanan <craig.buchanan@choa.org>
2015-06-25 15:51:33 -0400

    initial commit

---
+++ Functions/Get-PropertyValue.Tests.ps1
@@ -0,0 +1,21 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Get-PropertyValue" {
+
+    # arrange
+    $url='http://apps/ReportRequests'
+    $list='ReportRequests'
+    $id=7353
+    $property='ActualHoursDone'
+
+    It "Should returns a field's value" {
+        # act
+        $Response = Get-PropertyValue -WebUrl $url -listName $list -itemId $id -property $property -verbose
+
+        # asset
+        $Response.StatusCode | Should Be 200
+    }
+
+}
---
+++ Functions/Get-PropertyValue.ps1
@@ -0,0 +1,46 @@
+﻿function Get-PropertyValue
+{
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$WebUrl,
+
+        [Parameter(Mandatory=$True)]
+        [string]$ListName,
+
+        [Parameter(Mandatory=$True)]
+        [int]$ItemId, 
+
+        [Parameter(Mandatory=$True)]
+        [string]$Property
+    )
+
+  BEGIN { Write-Verbose "$($MyInvocation.MyCommand.Name)::Begin" }
+
+  PROCESS {
+    Write-Verbose "$($MyInvocation.MyCommand.Name)::Process"
+
+    $endpointUrl = "$WebUrl/_vti_bin/listdata.svc/$ListName($ItemId)/$Property/\`$value"
+    Write-Verbose "Endpoint Url: $endpointurl"
+    
+    try {
+        $response = Invoke-WebRequest -Uri $endpointUrl -Method Get -UseDefaultCredentials
+        Write-Verbose "Content: $($Response.Content)"
+    }
+
+    # Invoke-WebRequest throws System.Net.WebException
+    catch [System.Net.WebException] {
+        throw
+    }
+
+    finally {
+        # return response object
+        $response
+    }
+
+  } #PROCESS
+
+  END {Write-Verbose "$($MyInvocation.MyCommand.Name)::End"}
+
+}
