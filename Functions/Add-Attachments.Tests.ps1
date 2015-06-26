cc2b6840fa4adf778c9b53e9f1763f1b25d2b6e6
Craig Buchanan <craig.buchanan@choa.org>
2015-06-26 08:47:20 -0400

    Adjusting test

--- Functions/Add-Attachments.Tests.ps1
+++ Functions/Add-Attachments.Tests.ps1
@@ -8,12 +8,15 @@ Describe "Add-Attachments" {
     $url='http://apps/ReportRequests/'
     $list='ReportRequests'
     $id=7579
-    $path='C:\Users\cb2215\Desktop\7579.txt'
 
-    It "Should attach a new file" {
+    $content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.'
+    $path = "TestDrive:\file.txt"
+    Set-Content $path -value $content
+
+    It -Skip "Should attach a new file" {
 
         # act
-        $Response = Update-Attachments -WebUrl $url -ListName $list -ItemId $id -Paths $path -Verbose
+        $Response = Add-Attachments -WebUrl $url -ListName $list -ItemId $id -Paths $path -Verbose
 
         # assert
         $Response.StatusCode | Should Be 201 # Created
