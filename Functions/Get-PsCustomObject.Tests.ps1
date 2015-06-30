a6af85e71630a93c9fe35dbf8884023e47b1b7f3
Craig Buchanan <craig.buchanan@choa.org>
2015-06-29 21:33:19 -0400

    initial commit

---
+++ Functions/Get-PsCustomObject.Tests.ps1
@@ -0,0 +1,20 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Get-PsCustomObject" {
+
+  $EndpointUrl='http://apps/ReportRequests'
+  $ListName='ReportRequests'
+  $ItemId=7579
+
+  It "Should return a PsCustomObject" {
+      # act
+      $actual = Get-PsCustomObject -Url "$EndpointUrl/_vti_bin/listdata.svc/$ListName($ItemId)"
+
+      # assert
+      ($actual).GetType() | Should Be System.Management.Automation.PsCustomObject
+
+  }
+
+}
---
+++ Functions/Get-PsCustomObject.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.SYNOPSIS
+Return a SharePoint resource as a PsCustomObject
+
+.PARAMETER Url
+The Url of the JSON resource.
+
+.EXAMPLE
+PS> Get-PsCustomObject -Url 'http://contoso.intranet.com/_vti_bin/listdata.svc/TheList(1)''
+
+.OUTPUT
+PsCustomObject
+
+#>
+Function Get-PsCustomObject {
+
+    Param(
+      [Parameter(Mandatory=$True,Position=1)]
+      [string]$Url
+    )
+
+    try {
+
+      $client = New-Object System.Net.WebClient
+      $client.Credentials = [System.Net.CredentialCache]::DefaultCredentials 
+      $client.Headers.Add("Content-Type", "application/json;odata=verbose")
+      $client.Headers.Add("Accept", "application/json;odata=verbose")
+
+      $data = $client.DownloadString($Url)
+      # return PsCustomObject
+      return $data | ConvertFrom-Json
+
+    }
+    catch {
+      throw
+    }
+    finally {
+      $client.Dispose()
+    }
+
+}
