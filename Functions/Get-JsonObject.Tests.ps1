9980e0ec466c1357fe4359f09710e7bac2042fd2
Craig Buchanan <craig.buchanan@choa.org>
2015-07-10 11:40:11 -0400

    initial commit

---
+++ Functions/Get-JsonObject.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Get-JsonObject" {
+
+    # arrange
+    $EndpointUrl='http://apps/ReportRequests'
+    $ListName='ReportRequests'
+    $ItemId=7579
+
+    It "Should return a Json-formatted string" {
+
+        # act
+        $actual = Get-JsonObject -Url "$EndpointUrl/_vti_bin/listdata.svc/$ListName($ItemId)"
+
+        # assert
+        ($actual).GetType() | Should Be String
+        ($actual | ConvertFrom-Json).GetType() | Should Be System.Management.Automation.PsCustomObject
+
+    }
+
+}
---
+++ Functions/Get-JsonObject.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+Return a SharePoint resource as a JSON string
+
+.PARAMETER Url
+The Url of the JSON resource.
+
+.EXAMPLE
+PS> Get-JsonObject -Url 'http://contoso.intranet.com/_vti_bin/listdata.svc/TheList(1)''
+
+.OUTPUT
+Json-formatted string
+
+#>
+function Get-JsonObject {
+
+    Param(
+      [Parameter(Mandatory=$True,Position=1)]
+      [string]$Url
+    )
+
+    try {
+
+      $client = New-Object System.Net.WebClient
+      $client.Credentials = [System.Net.CredentialCache]::DefaultCredentials 
+      $client.Headers.Add("Content-Type", "application/json;odata=verbose")
+      $client.Headers.Add("Accept", "application/json;odata=verbose")
+      $client.DownloadString($Url)
+
+    }
+    catch {
+      throw
+    }
+    finally {
+      $client.Dispose()
+    }
+
+}
